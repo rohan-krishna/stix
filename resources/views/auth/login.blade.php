fe69e403169d1a9ac3d8ea1a5783665c25063057
rohan-krishna <phonemg30993@gmail.com>
2016-10-02 11:58:13 -0700

    hide pass reset link --submit btn is also gone

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,17 +48,17 @@
                             </div>
                         </div>
 
-                       {{--  <div class="form-group">
+                        <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                                {{-- <a class="btn btn-link" href="{{ url('/password/reset') }}">
                                     Forgot Your Password?
-                                </a>
+                                </a> --}}
                             </div>
-                        </div> --}}
+                        </div>
                     </form>
                 </div>
             </div>
